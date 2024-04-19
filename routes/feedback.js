335cf8172945e6b8f73ff8a627f6faefb2315506
Manjunath HK <manjunathhk@gmail.com>
2024-04-20 00:08:00 +0530

    Listing of feedback from the data folder.

--- routes/feedback.js
+++ routes/feedback.js
@@ -7,7 +7,11 @@ module.exports = (params) => {
   router.get('/', async (request, response, next) => {
     try {
       const feedback = await feedbackService.getList();
-      return response.json(feedback);
+      return response.render('layout', {
+        pageTitle: 'Feedback',
+        template: 'feedback',
+        feedback,
+      });
     } catch (error) {
       return next(error);
     }
